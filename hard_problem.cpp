61d1d1cc67f86e2b50b6e1062d844d6b58bce404
slowikj <jadwis.slowik@gmail.com>
2016-09-12 15:23:19 +0200

    Divide the Solve() procedure into several smaller ones

--- hard_problem.cpp
+++ hard_problem.cpp
@@ -4,7 +4,11 @@
 
 using namespace std;
 
-#define debug(x) cerr << #x << " = " << (x) << endl
+const int LAST_NORM = 0;
+const int LAST_REV = 1;
+const int MAXN = 1e5;
+const int MAXCOST = 1e9;
+const long long INF = (long long)MAXN * MAXCOST + 10;
 
 void ReadStrings (const int& n, string str[])
 {
@@ -18,43 +22,67 @@ void ReadCosts (const int& n, int cost[])
 		cin >> cost[i];
 }
 
-void Solve (const int& n, string str[], int cost[])
+void ComputeForUnreversedEnd (const int& prefixEnd,
+							  const string& currentStr,
+							  const string& previousStr, const string& reversedPrevStr,
+							  long long res[][2])
 {
-	const int LAST_NORM = 0;
-	const int LAST_REV = 1;
-	const int MAXN = 1e5;
-	const int MAXCOST = 1e9;
-	const long long INF = (long long)MAXN * MAXCOST + 10;
+	if (currentStr >= previousStr)
+		res[prefixEnd][LAST_NORM] = min(res[prefixEnd][LAST_NORM], res[prefixEnd - 1][LAST_NORM]);
+	if (currentStr >= reversedPrevStr)
+		res[prefixEnd][LAST_NORM] = min(res[prefixEnd][LAST_NORM], res[prefixEnd - 1][LAST_REV]);
+}
 
-	long long res[n][2];
+void ComputeForReversedEnd (const int& prefixEnd,
+							const string& reversedCurrentStr,
+							const string& previousStr, const string& reversedPreviousStr,
+							int cost[], long long res[][2])
+{
+	if (reversedCurrentStr >= previousStr)
+		res[prefixEnd][LAST_REV] = min(res[prefixEnd][LAST_REV], res[prefixEnd - 1][LAST_NORM] + cost[prefixEnd]);
+	if (reversedCurrentStr >= reversedPreviousStr)
+		res[prefixEnd][LAST_REV] = min(res[prefixEnd][LAST_REV], res[prefixEnd - 1][LAST_REV] + cost[prefixEnd]);
+}
+
+void ComputeReversedStrings (const int& n, string str[], string revStr[])
+{
+	for (int i = 0; i < n; ++i)
+	{
+		revStr[i] = str[i];
+		reverse (revStr[i].begin(), revStr[i].end());
+	}
+}
+
+void AssignInitialValues (const int& n, int cost[], long long res[][2])
+{
+	for (int i = 0; i < n; ++i)
+		res[i][0] = res[i][1] = INF;
 
 	res[0][LAST_NORM] = 0;
 	res[0][LAST_REV] = cost[0];
+}
+
+long long GetResult (const int& n, string str[], int cost[])
+{
+	long long res[n][2];
+	AssignInitialValues(n, cost, res);
+
+	string revStr[n];
+	ComputeReversedStrings(n, str, revStr);
 
 	for (int i = 1; i < n; ++i)
 	{
-		string strRev = str[i];
-		reverse (strRev.begin(), strRev.end());
-
-		string prevStrRev = str[i - 1];
-		reverse (prevStrRev.begin(), prevStrRev.end());
-
-		res[i][LAST_NORM] = res[i][LAST_REV] = INF;
-
-		if (str[i] >= str[i - 1])
-			res[i][LAST_NORM] = min(res[i][LAST_NORM], res[i - 1][LAST_NORM]);
-		if (str[i] >= prevStrRev)
-			res[i][LAST_NORM] = min(res[i][LAST_NORM], res[i - 1][LAST_REV]);
-		
-		if (strRev >= str[i - 1])
-			res[i][LAST_REV] = min(res[i][LAST_REV], res[i - 1][LAST_NORM] + cost[i]);
-		if (strRev >= prevStrRev)
-			res[i][LAST_REV] = min(res[i][LAST_REV], res[i - 1][LAST_REV] + cost[i]);
+		ComputeForUnreversedEnd(i, str[i], str[i - 1], revStr[i - 1], res);
+		ComputeForReversedEnd(i, revStr[i], str[i - 1], revStr[i - 1], cost, res); 
 	}
 
-	long long finalResult = min(res[n - 1][LAST_NORM], res[n - 1][LAST_REV]);
+	return min(res[n - 1][LAST_NORM], res[n - 1][LAST_REV]);
+}
 
-	cout << (finalResult == INF ? -1 : finalResult)  << endl;
+void Solve (const int& n, string str[], int cost[])
+{
+	long long res = GetResult(n, str, cost);
+	cout << (res == INF ? -1 : res) << endl;
 }
 
 int main ()
