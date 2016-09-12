efbfb0aff372c1e13b414ab70afe84a9a84ab9e7
slowikj <jadwis.slowik@gmail.com>
2016-09-12 23:05:48 +0200

    Add the class TaskSolver

--- hard_problem.cpp
+++ hard_problem.cpp
@@ -4,11 +4,155 @@
 
 using namespace std;
 
-const int LAST_NORM = 0;
-const int LAST_REV = 1;
-const int MAXN = 1e5;
-const int MAXCOST = 1e9;
-const long long INF = (long long)MAXN * MAXCOST + 10;
+class TaskSolver
+{
+private:
+	static const int LAST_NORM = 0;
+	static const int LAST_REV = 1;
+	static const int MAXN = 1e5;
+	static const int MAXCOST = 1e9;
+	static const int NUMBER_OF_STATES = 2;
+	static const long long INF = (long long)MAXN * MAXCOST + 10;
+
+	int *cost;
+	string *str;
+	string *revStr;
+	int numberOfStrings;
+	long long **res;
+
+	void CopyStrings (const int& numberOfStrings, string *str)
+	{
+		this->str = new string[numberOfStrings];
+		for (int i = 0; i < numberOfStrings; ++i)
+			this->str[i] = str[i];
+	}
+
+	void CopyCosts (const int& numberOfStrings, int *cost)
+	{
+		this->cost = new int[numberOfStrings];
+		for (int i = 0; i < numberOfStrings; ++i)
+			this->cost[i] = cost[i];
+	}		
+
+public:
+	TaskSolver (const int& numberOfStrings, int *cost, string *str)
+	{
+		this->numberOfStrings = numberOfStrings;
+		this->CopyStrings(numberOfStrings, str);
+		this->CopyCosts(numberOfStrings, cost);
+
+		this->revStr = nullptr;
+		this->res = nullptr;
+	}
+
+	~TaskSolver ()
+	{
+		delete[] this->cost;
+		delete[] this->str;
+
+		if (this->revStr)
+			delete[] this->revStr;
+
+		if (this->res)
+		{
+			for (int i = 0; i < this->numberOfStrings; ++i)
+				if (this->res[i])
+					delete[] this->res[i];
+		}
+	}
+
+private:
+	void ComputeForUnreversedEnd (const int& endIndex)
+	{
+		if (this->str[endIndex] >= this->str[endIndex - 1])
+			this->res[endIndex][LAST_NORM] = min(this->res[endIndex][LAST_NORM],
+												 this->res[endIndex - 1][LAST_NORM]);
+
+		if (this->str[endIndex] >= this->revStr[endIndex - 1])
+			this->res[endIndex][LAST_NORM] = min(this->res[endIndex][LAST_NORM],
+												 this->res[endIndex - 1][LAST_REV]);
+	}	
+
+	void ComputeForReversedEnd (const int& endIndex)
+	{
+		if (this->revStr[endIndex] >= this->str[endIndex - 1])
+			this->res[endIndex][LAST_REV] = min(this->res[endIndex][LAST_REV],
+												this->res[endIndex - 1][LAST_NORM] + this->cost[endIndex]);
+
+		if (this->revStr[endIndex] >= this->revStr[endIndex - 1])
+			this->res[endIndex][LAST_REV] = min(this->res[endIndex][LAST_REV],
+												this->res[endIndex - 1][LAST_REV] + this->cost[endIndex]);
+	}
+
+	void ComputeReversedStrings ()
+	{
+		for (int i = 0; i < this->numberOfStrings; ++i)
+		{
+			this->revStr[i] = this->str[i];
+			reverse (this->revStr[i].begin(), this->revStr[i].end());
+		}
+	}
+
+	void AssignInitialValues ()
+	{
+		for (int i = 0; i < this->numberOfStrings; ++i)
+			this->res[i][0] = this->res[i][1] = INF;
+
+		this->res[0][LAST_NORM] = 0;
+		this->res[0][LAST_REV] = this->cost[0];
+	}
+
+	void CreateResArray ()
+	{
+		this->res = new long long*[this->numberOfStrings];
+		
+		for (int i = 0; i < this->numberOfStrings; ++i)
+			this->res[i] = new long long[NUMBER_OF_STATES];
+	}
+
+	void CreateRevStrArray()
+	{
+		this->revStr = new string[this->numberOfStrings];
+	}
+
+	void Initialize ()
+	{
+		this->CreateResArray();
+		this->AssignInitialValues();
+
+		this->CreateRevStrArray();
+		this->ComputeReversedStrings();
+	}
+
+	void ComputeResult ()
+	{
+		this->Initialize();
+		
+		for (int i = 1; i < this->numberOfStrings; ++i)
+		{
+			this->ComputeForUnreversedEnd(i);
+			this->ComputeForReversedEnd(i);
+		}
+	}
+
+	bool ResultIsNotComputed ()
+	{
+		return this->res == nullptr;
+	}
+
+public:
+	long long GetResult ()
+	{
+		if (this->ResultIsNotComputed())
+			this->ComputeResult();
+	
+		long long tmpTotalRes = min(res[this->numberOfStrings - 1][LAST_NORM],
+								    res[this->numberOfStrings - 1][LAST_REV]);
+
+		return (tmpTotalRes == INF ? (long long)-1 : tmpTotalRes);
+	}
+};
+
 
 void ReadStrings (const int& n, string str[])
 {
@@ -22,69 +166,6 @@ void ReadCosts (const int& n, int cost[])
 		cin >> cost[i];
 }
 
-void ComputeForUnreversedEnd (const int& prefixEnd,
-							  const string& currentStr,
-							  const string& previousStr, const string& reversedPrevStr,
-							  long long res[][2])
-{
-	if (currentStr >= previousStr)
-		res[prefixEnd][LAST_NORM] = min(res[prefixEnd][LAST_NORM], res[prefixEnd - 1][LAST_NORM]);
-	if (currentStr >= reversedPrevStr)
-		res[prefixEnd][LAST_NORM] = min(res[prefixEnd][LAST_NORM], res[prefixEnd - 1][LAST_REV]);
-}
-
-void ComputeForReversedEnd (const int& prefixEnd,
-							const string& reversedCurrentStr,
-							const string& previousStr, const string& reversedPreviousStr,
-							int cost[], long long res[][2])
-{
-	if (reversedCurrentStr >= previousStr)
-		res[prefixEnd][LAST_REV] = min(res[prefixEnd][LAST_REV], res[prefixEnd - 1][LAST_NORM] + cost[prefixEnd]);
-	if (reversedCurrentStr >= reversedPreviousStr)
-		res[prefixEnd][LAST_REV] = min(res[prefixEnd][LAST_REV], res[prefixEnd - 1][LAST_REV] + cost[prefixEnd]);
-}
-
-void ComputeReversedStrings (const int& n, string str[], string revStr[])
-{
-	for (int i = 0; i < n; ++i)
-	{
-		revStr[i] = str[i];
-		reverse (revStr[i].begin(), revStr[i].end());
-	}
-}
-
-void AssignInitialValues (const int& n, int cost[], long long res[][2])
-{
-	for (int i = 0; i < n; ++i)
-		res[i][0] = res[i][1] = INF;
-
-	res[0][LAST_NORM] = 0;
-	res[0][LAST_REV] = cost[0];
-}
-
-long long GetResult (const int& n, string str[], int cost[])
-{
-	long long res[n][2];
-	AssignInitialValues(n, cost, res);
-
-	string revStr[n];
-	ComputeReversedStrings(n, str, revStr);
-
-	for (int i = 1; i < n; ++i)
-	{
-		ComputeForUnreversedEnd(i, str[i], str[i - 1], revStr[i - 1], res);
-		ComputeForReversedEnd(i, revStr[i], str[i - 1], revStr[i - 1], cost, res); 
-	}
-
-	return min(res[n - 1][LAST_NORM], res[n - 1][LAST_REV]);
-}
-
-void Solve (const int& n, string str[], int cost[])
-{
-	long long res = GetResult(n, str, cost);
-	cout << (res == INF ? -1 : res) << endl;
-}
-
 int main ()
 {
 	ios::sync_with_stdio(false);
@@ -96,7 +177,8 @@ int main ()
 	ReadCosts(n, cost);
 	ReadStrings(n, str);
 
-	Solve(n, str, cost);
+	TaskSolver ts(n, cost, str);
+	cout << ts.GetResult() << endl;
 
 	return 0;
 }
